f0e81ed1275ff23eb0e692f08b48648a57904c1f
Emanuel Fonseca <emdfonseca@codeplating.com>
2024-12-05 20:53:32 +0000

    chore: assume fluxcd repo already exists

--- cluster/infra-as-code/fluxcd/main.tf
+++ cluster/infra-as-code/fluxcd/main.tf
@@ -1,13 +1,4 @@
-resource "github_repository" "this" {
-  name        = var.github_repository
-  description = var.github_repository
-  visibility  = "private"
-  auto_init   = true # This is extremely important as flux_bootstrap_git will not work without a repository that has been initialised
-}
-
 resource "flux_bootstrap_git" "this" {
-  depends_on = [github_repository.this]
-
   embedded_manifests = true
   path               = var.fluxcd_path
 }
